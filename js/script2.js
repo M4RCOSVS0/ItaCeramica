36ed60270c71ecc856c2cca34e9df33ae2c4f56b
M4RCOSVS0 <marcossvin.so@gmail.com>
2024-11-08 09:59:32 -0300

    add

--- js/script2.js
+++ js/script2.js
@@ -1,21 +1,20 @@
-$(document).ready(function() {
-    $(document).ready(function() {
-        function checkWidth() {
-            if ($(window).width() <= 1280) {
-                $(".cat").hide(); // Oculta o .cat
-                $(".prod").css("display", "inline-block"); // Exibe o .prod lado a lado
-            } else {
-                $(".cat").css("display", "inline-block"); // Exibe o .cat lado a lado
-                $(".prod").hide(); // Oculta o .prod
-            }
+
+$(document).ready(function () {
+    function checkWidth() {
+        if ($(window).width() <= 1280) {
+            $(".cat").hide(); // Oculta o .cat
+            $(".prod").css("display", "inline-block"); // Exibe o .prod lado a lado
+        } else {
+            $(".cat").css("display", "inline-block"); // Exibe o .cat lado a lado
+            $(".prod").hide(); // Oculta o .prod
         }
+    }
 
-        // Executa a função quando a página carrega
+    // Executa a função quando a página carrega
+    checkWidth();
+
+    // Vincula a função ao redimensionamento da janela
+    $(window).resize(function () {
         checkWidth();
-
-        // Vincula a função ao redimensionamento da janela
-        $(window).resize(function() {
-            checkWidth();
-        });
     });
 });
